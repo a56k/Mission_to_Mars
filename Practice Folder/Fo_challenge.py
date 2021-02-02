3b1edd85d6eb90471039f85e398060b94e9c4059
Andy Mulumba <a56k@Andys-MBP.lan>
2021-02-01 20:48:21 -0800

    adding files for this weeks' module

---
+++ Practice Folder/Fo_challenge.py
@@ -0,0 +1,34 @@
+# ## JPL Space Images Featured Image
+
+# Visit URL
+url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+browser.visit(url)
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+# find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+# Use the base url to create an absolute url
+img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+img_url
+
+# ## Mars Facts
+
+df = pd.read_html('http://space-facts.com/mars/')[0]
+df.head()
+
+df.columns=['Description', 'Mars']
+df.set_index('Description', inplace=True)
+df
+
+df.to_html()
+
+browser.quit()
